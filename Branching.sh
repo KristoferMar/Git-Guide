3c9124b0951606fed3913aec34416f546160195c
KristoferMar <kristofer@brostu.dk>
2020-03-20 12:01:07 +0100

    branching notes

---
+++ Branching.sh
@@ -0,0 +1,14 @@
+# Create a Git branch
+# 1
+git fetch && git checkout <feature>
+or
+git checkout -b <feature>
+
+# 2
+git add .
+git commig -m "adding a change from the feature branch"
+git push origin <feature>
+
+# Switch branch
+git checkout <branch name>
+
